69d0d14502926c66a9bed78ba8f6bd0c7195bbc3
Gabe Koss <gabe@gabekoss.com>
2016-01-05 12:08:21 -0500

    tiniest refactor II

--- spec/device_spec.rb
+++ spec/device_spec.rb
@@ -5,7 +5,10 @@ describe BlueHydra::Device do
   it "has useful attributes" do
     device = BlueHydra::Device.new
 
-    %w{ id address}.each do |attr|
+    %w{
+      id
+      address
+    }.each do |attr|
       expect(device.respond_to?(attr)).to eq(true)
     end
   end
