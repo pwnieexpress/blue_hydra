c4db18a2ee4904253cf5a5abb284c17159a6e539
Gabe Koss <gabe@gabekoss.com>
2016-01-12 10:10:32 -0500

    How well do you know the characters from this config file?

--- lib/blue_hydra.rb
+++ lib/blue_hydra.rb
@@ -24,8 +24,28 @@ DataMapper.setup(
 )
 
 module BlueHydra
-  # 0.0.1 -- initial build out using
   VERSION = '0.0.1'
+
+  CONFIG_FILE = if Dir.exists?('/var/log/pwnix/pwnix-config')
+              File.expand_path('/var/log/pwnix/pwnix-config/blue_hydra.json', __FILE__)
+            else
+              File.expand_path('../../blue_hydra.json', __FILE__)
+            end
+
+  DEFAULT_CONFIG = {
+    log_level: "debug",
+    bt_device: "hci0" # TODO - make use of...?
+  }
+
+  @@config = if File.exists?(CONFIG_FILE)
+               DEFAULT_CONFIG.merge(JSON.parse(
+                 File.read(CONFIG_FILE),
+                 symbolize_names: true
+               ))
+             else
+               # TODO update config with defaults
+               DEFAULT_CONFIG
+             end
 
   LOGFILE = if Dir.exists?('/var/log/pwnix')
               File.expand_path('/var/log/pwnix/blue_hydra.log', __FILE__)
@@ -34,10 +54,27 @@ module BlueHydra
             end
 
   @@logger = Logger.new(LOGFILE)
-  @@logger.level = Logger::DEBUG
+  @@logger.level = case @@config[:log_level]
+                   when "fatal"
+                     Logger::FATAL
+                   when "error"
+                     Logger::ERROR
+                   when "warn"
+                     Logger::WARN
+                   when "info"
+                     Logger::INFO
+                   when "debug"
+                     Logger::DEBUG
+                   else
+                     Logger::INFO
+                   end
 
   def logger
     @@logger
+  end
+
+  def config
+    @@config
   end
 
   module_function :logger
