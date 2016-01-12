c94c8b0a256de01619f23aee9b9ceee93b01a422
Gabe Koss <gabe@gabekoss.com>
2016-01-12 16:41:57 -0500

    parser error, readd device logging

--- lib/blue_hydra/parser.rb
+++ lib/blue_hydra/parser.rb
@@ -244,8 +244,8 @@ module BlueHydra
       when line =~ /^RSSI:/
         set_attr("#{bt_mode}_rssi".to_sym, line.split(': ')[1])
 
-      when line =~ /^last_seen_at:/
-        set_attr(:last_seen_at, line.split(': ')[1].to_i)
+      when line =~ /^last_seen:/
+        set_attr(:last_seen, line.split(': ')[1].to_i)
 
       when line =~ /^(Attribute (data length|group list)|Reason|Result):/
         # do nothing
