8e159cf1afcad372f7d0f838401cd5ab2c37bf06
Gabe Koss <gabe@gabekoss.com>
2016-01-12 16:28:30 -0500

    updates for command stuff

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -5,7 +5,7 @@ describe BlueHydra::Command do
     result = BlueHydra::Command.execute3("echo 'hello world'")
     expect(result[:exit_code]).to eq(0)
     expect(result[:stdout]).to eq("hello world")
-    expect(result[:stderr]).to eq("")
+    expect(result[:stderr]).to eq(nil)
   end
 end
 
