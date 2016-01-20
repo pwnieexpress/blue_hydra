4c615fbb85b2e52df7893efd2b883770e2a52887
Gabe Koss <gabe@gabekoss.com>
2016-01-20 14:52:16 -0500

    s/debug/warn/

--- lib/blue_hydra/device.rb
+++ lib/blue_hydra/device.rb
@@ -1,4 +1,4 @@
-cilass BlueHydra::Device
+class BlueHydra::Device
   MAC_REGEX    = /^((?:[0-9a-f]{2}[:-]){5}[0-9a-f]{2})$/i
 
   include DataMapper::Resource
@@ -110,7 +110,7 @@ cilass BlueHydra::Device
     }.map(&:to_sym).each do |attr|
       if result[attr]
         if result[attr].uniq.count > 1
-          BlueHydra.logger.debug(
+          BlueHydra.logger.warn(
             "#{address} multiple values detected for #{attr}: #{result[attr].inspect}. Using first value..."
           )
         end
