f8425bcd709b474a68a7be0c149f0d0dfb1da472
Gabe Koss <gabe@gabekoss.com>
2016-02-11 14:40:28 -0500

    force ubertooth seeing a device to update the last_seen timestamps

--- lib/blue_hydra/runner.rb
+++ lib/blue_hydra/runner.rb
@@ -250,6 +250,12 @@ module BlueHydra
                 ubertooth_output[:stdout].each_line do |line|
                   if line =~ /^[\?:]{6}[0-9a-f:]{11}/i
                     address = line.scan(/^((\?\?:){2}([0-9a-f:]*))/i).flatten.first.gsub('?', '0')
+
+                    result_queue.push({
+                      address: [address],
+                      last_seen: [Time.now.to_i]
+                    })
+
                     BlueHydra.logger.debug("Ubertooth line #{line.strip}")
                     BlueHydra.logger.debug("Ubertooth pushing #{address} to queue")
                     push_to_queue(:classic, address)
@@ -396,11 +402,13 @@ module BlueHydra
               }.each do |device|
                 self.query_history[device.address] ||= {}
                 if (Time.now.to_i - (60 * 7)) >= self.query_history[device.address][:l2ping].to_i
+
                   # BlueHydra.logger.debug("device l2ping scan triggered")
                   l2ping_queue.push({
                     command: :l2ping,
                     address: device.address
                   })
+
                   self.query_history[device.address][:l2ping] = Time.now.to_i
                 end
               end
