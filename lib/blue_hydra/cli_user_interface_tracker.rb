e830184588cfa925a723222e36aa8a3a088c6567
Gabe Koss <gabe@gabekoss.com>
2016-06-22 16:51:06 -0400

    track cui using new attrs :(

--- lib/blue_hydra/cli_user_interface_tracker.rb
+++ lib/blue_hydra/cli_user_interface_tracker.rb
@@ -1,12 +1,45 @@
+require 'securerandom'
+
 module BlueHydra
   class CliUserInterfaceTracker
-    attr_accessor :runner, :chunk, :attrs, :address
+    attr_accessor :runner, :chunk, :attrs, :address, :uuid
 
     def initialize(run, chnk, attrs, addr)
       @runner = run
       @chunk = chnk
       @attrs = attrs
       @address = addr
+
+      @lpu  = attrs[:le_proximity_uuid].first if attrs[:le_proximity_uuid]
+      @lmn  = attrs[:le_major_num].first      if attrs[:le_major_num]
+      @lmn2 = attrs[:le_minor_num].first      if attrs[:le_minor_num]
+
+      cui_k = cui_status.keys
+      cui_v = cui_status.values
+
+      match1 = cui_v.select{|x|
+        x[:address] == @address
+      }.first
+
+      if match1
+        @uuid = cui_k[cui_v.index(match1)]
+      end
+
+      unless @uuid
+        match2 = cui_v.select{|x|
+          x[:le_proximity_uuid] && x[:le_proximity_uuid] == @lpu &&
+          x[:le_major_num]      && x[:le_major_num]      == @lmn &&
+          x[:le_minor_num]      && x[:le_minor_num]      == @lmn2
+        }.first
+
+        if match2
+          @uuid = cui_k[cui_v.index(match2)]
+        end
+      end
+
+      unless @uuid
+        @uuid = SecureRandom.uuid
+      end
     end
 
     def cui_status
@@ -14,8 +47,8 @@ module BlueHydra
     end
 
     def update_cui_status
-      cui_status[address] ||= {created: Time.now.to_i}
-      cui_status[address][:lap] = address.split(":")[3,3].join(":") unless cui_status[address][:lap]
+      cui_status[@uuid] ||= {created: Time.now.to_i}
+      cui_status[@uuid][:lap] = address.split(":")[3,3].join(":") unless cui_status[@uuid][:lap]
 
       if chunk[0] && chunk[0][0]
         bt_mode = chunk[0][0] =~ /^\s+LE/ ? "le" : "classic"
@@ -23,56 +56,57 @@ module BlueHydra
 
       if bt_mode == "le"
         if attrs[:lmp_version] && attrs[:lmp_version].first !~ /0x(00|FF|ff)/
-          cui_status[address][:vers] = "LE#{attrs[:lmp_version].first.split(" ")[1]}"
-        elsif !cui_status[address][:vers]
-          cui_status[address][:vers] = "BTLE"
+          cui_status[@uuid][:vers] = "LE#{attrs[:lmp_version].first.split(" ")[1]}"
+        elsif !cui_status[@uuid][:vers]
+          cui_status[@uuid][:vers] = "BTLE"
         end
       else
         if attrs[:lmp_version] && attrs[:lmp_version].first !~ /0x(00|ff|FF)/
-          cui_status[address][:vers] = "CL#{attrs[:lmp_version].first.split(" ")[1]}"
-        elsif !cui_status[address][:vers]
-          cui_status[address][:vers] = "CL/BR"
+          cui_status[@uuid][:vers] = "CL#{attrs[:lmp_version].first.split(" ")[1]}"
+        elsif !cui_status[@uuid][:vers]
+          cui_status[@uuid][:vers] = "CL/BR"
         end
       end
 
       [
-        :last_seen, :name, :address, :classic_rssi, :le_rssi
+        :last_seen, :name, :address, :classic_rssi, :le_rssi,
+        :le_proximity_uuid, :le_major_num, :le_minor_num
       ].each do |key|
         if attrs[key] && attrs[key].first
-          if cui_status[address][key] != attrs[key].first
+          if cui_status[@uuid][key] != attrs[key].first
             if key == :le_rssi || key == :classic_rssi
-              cui_status[address][:rssi] = attrs[key].first[:rssi].gsub('dBm','')
+              cui_status[@uuid][:rssi] = attrs[key].first[:rssi].gsub('dBm','')
             else
-              cui_status[address][key] = attrs[key].first
+              cui_status[@uuid][key] = attrs[key].first
             end
           end
         end
       end
 
       if attrs[:short_name]
-        unless attrs[:short_name] == [nil] || cui_status[address][:name]
-          cui_status[address][:name] = attrs[:short_name].first
+        unless attrs[:short_name] == [nil] || cui_status[@uuid][:name]
+          cui_status[@uuid][:name] = attrs[:short_name].first
           BlueHydra.logger.warn("short name found: #{attrs[:short_name]}")
         end
       end
 
       if attrs[:appearance]
-        cui_status[address][:type] = attrs[:appearance].first.split('(').first
+        cui_status[@uuid][:type] = attrs[:appearance].first.split('(').first
       end
 
       if attrs[:classic_minor_class]
         if attrs[:classic_minor_class].first =~ /Uncategorized/i
-          cui_status[address][:type] = "Uncategorized"
+          cui_status[@uuid][:type] = "Uncategorized"
         else
-          cui_status[address][:type] = attrs[:classic_minor_class].first.split('(').first
+          cui_status[@uuid][:type] = attrs[:classic_minor_class].first.split('(').first
         end
       end
 
-      if [nil, "Unknown"].include?(cui_status[address][:manuf])
+      if [nil, "Unknown"].include?(cui_status[@uuid][:manuf])
         if bt_mode == "classic" || (attrs[:le_address_type] && attrs[:le_address_type].first =~ /public/i)
             vendor = Louis.lookup(address)
 
-            cui_status[address][:manuf] = if vendor["short_vendor"]
+            cui_status[@uuid][:manuf] = if vendor["short_vendor"]
                                             vendor["short_vendor"]
                                           else
                                             vendor["long_vendor"]
@@ -91,7 +125,7 @@ module BlueHydra
           end
 
           if cmp
-            cui_status[address][:manuf] = cmp.split('(').first
+            cui_status[@uuid][:manuf] = cmp.split('(').first
           end
         end
       end
