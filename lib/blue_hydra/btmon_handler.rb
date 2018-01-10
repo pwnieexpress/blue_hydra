1a24065436beda6ad41d583a3d8a671b6e88fb76
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2018-01-10 15:30:06 -0500

    scope out local

--- lib/blue_hydra/btmon_handler.rb
+++ lib/blue_hydra/btmon_handler.rb
@@ -155,8 +155,10 @@ module BlueHydra
           # additional observed values include "ACL Connection Already Exists", "Command Disallowed"
           # "LMP Response Timeout / LL Response Timeout", "Connection Accept Timeout Exceeded"
           # "Connection Timeout"
-          (buffer[0] =~ /(Connect Complete|Remote Name Req)/ && buffer[1] !~ /Status: Success/ )
+          (buffer[0] =~ /(Connect Complete|Remote Name Req)/ && buffer[1] !~ /Status: Success/ ) ||
 
+          # Has it seriously been till now that we scope out the local calls?  How is that even possible? This must be dropped somewhere else but I can't find it.
+          (buffer[0] =~ /Read Local/ || buffer[1] =~ /Read Local/ )
         )
 
         # log used btmon output for review if we are in debug mode
