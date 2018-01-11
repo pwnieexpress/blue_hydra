6c212e42d0bd9331255f6c42363cf11691de34a3
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2018-01-11 12:13:52 -0500

    start of line for consistency

--- lib/blue_hydra/btmon_handler.rb
+++ lib/blue_hydra/btmon_handler.rb
@@ -140,7 +140,7 @@ module BlueHydra
           buffer.first =~ /^= Open Index:/                                                    ||
           buffer.first =~ /^= Close Index:/                                                   ||
           buffer.first =~ /^= Index Info:/                                                    ||
-          buffer.first =~ /= bluetoothd: Unable to/                                           ||
+          buffer.first =~ /^= bluetoothd: Unable to/                                          ||
           buffer.first =~ /^= Note:/                                                          ||
           (buffer[0] =~ /^> HCI Event: Command Compl.* \(0x0e\)/ && buffer[1] !~ /Remote/ )   ||
 
